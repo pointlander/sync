0b3835e1898140ca94bdd6e8f1fcb51a5251844d
Andrew Snodgrass <git@pointlander.info>
2019-04-19 23:40:00 -0600

    WIP spike

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/pointlander/sync
+
+require gonum.org/v1/plot v0.0.0-20190410204940-3a5f52653745
--- main.go
+++ main.go
@@ -8,8 +8,14 @@ import (
 	"fmt"
 	"image"
 	"image/png"
+	"math"
 	"math/rand"
 	"os"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
 )
 
 const (
@@ -19,8 +25,9 @@ const (
 )
 
 type CA struct {
-	Rule        uint8
-	State, Next []uint64
+	Rule    uint8
+	State   []uint64
+	Entropy float64
 }
 
 func NewCA(rule uint8, size int) CA {
@@ -31,7 +38,6 @@ func NewCA(rule uint8, size int) CA {
 	return CA{
 		Rule:  rule,
 		State: state,
-		Next:  make([]uint64, size),
 	}
 }
 
@@ -41,7 +47,8 @@ func main() {
 	for i := range nodes {
 		nodes[i] = NewCA(110, Chunks)
 	}
-	gray, count := image.NewGray(image.Rect(0, 0, 2 * CASize + 3, iterations)), 0
+	points := make(plotter.XYs, 0, iterations)
+	gray, count, next := image.NewGray(image.Rect(0, 0, 2*CASize+3, iterations)), 0, make([]uint64, Chunks)
 	for i := 0; i < iterations; i++ {
 		for n := range nodes {
 			for _, s := range nodes[n].State {
@@ -64,16 +71,18 @@ func main() {
 				count++
 			}
 		}
-		if rand.Float64() < .01 {
+		if rnd := rand.Float64() * 32; rnd < nodes[0].Entropy || rnd < nodes[1].Entropy {
 			//a, b := rand.Intn(Chunks), rand.Intn(Chunks)
 			//nodes[0].State[a], nodes[1].State[b] = nodes[1].State[b], nodes[0].State[a]
 			a := rand.Intn(Chunks)
 			nodes[0].State[a], nodes[1].State[a] = nodes[1].State[a], nodes[0].State[a]
+			fmt.Printf("fire: %d %f %f\n", i, nodes[0].Entropy, nodes[1].Entropy)
 		}
 		for n := range nodes {
-			nodes[n].Step()
+			next = nodes[n].Step(next)
 		}
-		fmt.Printf("iteration: %d\n", i)
+		points = append(points, plotter.XY{X: float64(i), Y: nodes[0].Entropy})
+		//fmt.Printf("iteration: %d %f %f\n", i, nodes[0].Entropy, nodes[1].Entropy)
 	}
 
 	out, err := os.Create("ca.png")
@@ -85,10 +94,32 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+
+	p.Title.Text = "entropy"
+	p.X.Label.Text = "time"
+	p.Y.Label.Text = "entropy"
+
+	scatter, err := plotter.NewScatter(points)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	p.Add(scatter)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "entropy.png")
+	if err != nil {
+		panic(err)
+	}
 }
 
-func (ca *CA) Step() {
-	rule, state, next := ca.Rule, ca.State, ca.Next
+func (ca *CA) Step(next []uint64) []uint64 {
+	rule, state, histogram := ca.Rule, ca.State, [8]uint64{}
 	length := len(state)
 	index, out := state[length-1]>>63, 0
 	for i, s := range state {
@@ -101,6 +132,7 @@ func (ca *CA) Step() {
 		next[i] = 0
 		for c > 0 {
 			index = ((index << 1) & 0x7) | (s & 0x1)
+			histogram[index]++
 			s >>= 1
 			next[out>>6] |= uint64((rule>>index)&0x1) << uint(out&0x3F)
 			c--
@@ -108,8 +140,17 @@ func (ca *CA) Step() {
 		}
 	}
 	index = ((index << 1) & 0x7) | (state[0] & 0x1)
+	histogram[index]++
 	next[out>>6] |= uint64((rule>>index)&0x1) << uint(out&0x3F)
-	ca.State, ca.Next = next, state
+	ca.State = next
+
+	entropy := 0.0
+	for _, i := range histogram {
+		p := float64(i) / 512
+		entropy += p * math.Log2(p)
+	}
+	ca.Entropy = math.Exp(entropy)
+	return state
 }
 
 func (ca *CA) String() string {
